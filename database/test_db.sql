7d440460f237218292dfc81ccd4031c25a45dd5e
Vašek <wasekva@gmail.com>
2022-04-17 14:58:20 +0200

    Updated selects

--- database/test_db.sql
+++ database/test_db.sql
@@ -333,7 +333,7 @@ INSERT INTO client_user VALUES (SEQ_CLIENT_USER_ID.nextval, '1682334', 'd566c130
 INSERT INTO client_user VALUES (SEQ_CLIENT_USER_ID.nextval, '1210801', '4a3ca91f11c4ade33cca71eabf4f179a9f74f05b09367612256aa9428bcf602e', 5);
 
 INSERT INTO account VALUES (SEQ_ACCOUNT_ID.nextval, '1030432706/6100', 'CZ7661000000001030432706', 531.20, 1, 1, 4, 1, 1, 2);
-INSERT INTO account VALUES (SEQ_ACCOUNT_ID.nextval, '670100/2216739313/6210', 'CZ5262106701002216739313', 2999.00, 0, 5, 3, 2, 1, 2);
+INSERT INTO account VALUES (SEQ_ACCOUNT_ID.nextval, '670100/2216739313/6210', 'CZ5262106701002216739313', 2999.00, 0, 5, 3, 2, 2, 2);
 INSERT INTO account VALUES (SEQ_ACCOUNT_ID.nextval, '000000/5134779323/0900', 'SK4709000000005134779323', 150020.90, 0, 4, 4, 1, 1, 1);
 
 INSERT INTO payment_card VALUES (SEQ_PAYMENT_CARD_ID.nextval, '4801769871971639', '07/25', '656', 1, 10000.00, 10000.00, 10000.00, 40000.00, 3, 5, 1);
@@ -363,5 +363,42 @@ INSERT INTO rules VALUES (SEQ_RULES_ID.nextval, 1000, 3, 2);
 INSERT INTO rules VALUES (SEQ_RULES_ID.nextval, 20, 3, 3);
 INSERT INTO rules VALUES (SEQ_RULES_ID.nextval, 50, 1, 1);
 INSERT INTO rules VALUES (SEQ_RULES_ID.nextval, 50, 2, 5);
+
+-- ###### dva dotazy využívající spojení dvou tabulek ######
+-- Zobrazenie podrobností o účte (účet/typ)
+SELECT ACCOUNT_NUMBER, IBAN, BALANCE, IS_ACTIVE, TYPE_NAME, MANAGEMENT_FEE
+    FROM account NATURAL JOIN account_type;
+
+-- Zobrazenie podrobností o platobnej karte (karta/typ karty)s
+SELECT CARD_NUMBER, EXPIRATION_DATE, CARD_TYPE.NAME as CARD_TYPE, COMPANY
+    FROM payment_card NATURAL JOIN card_type;
+
+-- ###### jeden využívající spojení tří tabulek ########
+-- Zobrazenie informácii o človeku.
+SELECT FIRST_NAME, LAST_NAME, PERSON_ID, GENDER, DATE_OF_BIRTH, PHONE_NUMBER, EMAIL, ADDRESS, CITY, COUNTRY, POSTAL_CODE
+    FROM person NATURAL JOIN place NATURAL JOIN contact_info;
+
+-- ###### dva dotazy s klauzulí GROUP BY a agregační funkcí #######
+-- Počet rôznych typov účtov
+SELECT TYPE_NAME, COUNT(*) as Accounts_count FROM ACCOUNT_TYPE NATURAL JOIN ACCOUNT GROUP BY TYPE_NAME;
+
+-- Počet vydaných kariet jednotlivými spoločnosťami.
+SELECT COMPANY, COUNT(*) as CARDS_COUNT FROM CARD_TYPE GROUP BY COMPANY;
+
+-- ###### jeden dotaz obsahující predikát EXISTS ######
+-- Zobrazenie služieb aktívnych účtov
+SELECT NAME, DESCRIPTION, FEE
+    FROM SERVICE WHERE EXISTS(
+        SELECT ACCOUNT_NUMBER FROM ACCOUNT
+        JOIN ACCOUNT_SERVICE AccService on ACCOUNT.ACCOUNT_ID = AccService.ACCOUNT_ID
+        WHERE SERVICE.SERVICE_ID = AccService.SERVICE_ID AND IS_ACTIVE = 1);
+
+-- ###### jeden dotaz s predikátem IN s vnořeným selectem (nikoliv IN s množinou konstantních dat) #######
+-- Zobrazenie základných informácií o zamestnancoch banky, ktorá má id nižšie ako 2
+SELECT FIRST_NAME, LAST_NAME, PERSONAL_ID, WORK_POSITION, SALARY FROM PERSON
+    JOIN EMPLOYEE ON PERSON.PERSON_ID = EMPLOYEE.PERSON_ID
+    JOIN BRANCH ON EMPLOYEE.BRANCH_ID = BRANCH.BRANCH_ID
+    JOIN BANK ON BANK.BANK_ID = BRANCH.BANK_ID
+    WHERE BANK.NAME IN (SELECT NAME FROM BANK WHERE BANK.BANK_CODE > 6000);
 
 COMMIT;

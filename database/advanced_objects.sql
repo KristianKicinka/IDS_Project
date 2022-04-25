e2cb8cd96f097d84feee394d7d323f870425dac0
Vašek <wasekva@gmail.com>
2022-04-25 18:15:34 +0200

    profi formating

--- database/advanced_objects.sql
+++ database/advanced_objects.sql
@@ -1,58 +1,62 @@
 -- vytvoření alespoň dvou netriviálních databázových triggerů vč. jejich předvedení
 
 
-
 -- vytvoření alespoň dvou netriviálních uložených procedur vč. jejich předvedení,
 -- ve kterých se musí (dohromady) vyskytovat alespoň jednou kurzor,
 -- ošetření výjimek a použití proměnné s datovým typem odkazujícím se na řádek či
 -- typ sloupce tabulky (table_name.column_name%TYPE nebo table_name%ROWTYPE),
 
-CREATE OR REPLACE PROCEDURE CHANGE_DEBITS_LIMITS_BY_COMPANY
-    (cardCompany varchar, debitLimit number)
-IS
+CREATE OR REPLACE PROCEDURE CHANGE_DEBITS_LIMITS_BY_COMPANY(cardCompany varchar, debitLimit number)
+    IS
     card_id CARD_TYPE.CARD_TYPE_ID%type;
-    CURSOR select_cards IS SELECT CARD_TYPE_ID FROM CARD_TYPE WHERE CARD_TYPE.COMPANY = cardCompany;
-    limit number;
+    CURSOR select_cards IS SELECT CARD_TYPE_ID
+                           FROM CARD_TYPE
+                           WHERE CARD_TYPE.COMPANY = cardCompany;
+    limit   number;
 BEGIN
-    limit:=debitLimit;
+    limit := debitLimit;
     IF (limit IS NULL) THEN
-        limit:=0;
-    ELSE limit:=debitLimit;
+        limit := 0;
+    ELSE
+        limit := debitLimit;
     END IF;
-   OPEN select_cards;
-   LOOP
+    OPEN select_cards;
+    LOOP
         FETCH select_cards INTO card_id;
         EXIT WHEN select_cards%NOTFOUND;
         UPDATE CARD_TYPE SET DEBIT_LIMIT = limit WHERE CARD_TYPE_ID = card_id;
         COMMIT;
     end loop;
-   CLOSE select_cards;
+    CLOSE select_cards;
 
 EXCEPTION
-WHEN OTHERS THEN
-   raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+    WHEN OTHERS THEN
+        raise_application_error(-20001, 'An error was encountered - ' || SQLCODE || ' -ERROR- ' || SQLERRM);
 END;
 /
 BEGIN
-    CHANGE_DEBITS_LIMITS_BY_COMPANY('VISA',NULL);
+    CHANGE_DEBITS_LIMITS_BY_COMPANY('VISA', NULL);
 END;
 /
 
-SELECT * FROM CARD_TYPE;
+SELECT *
+FROM CARD_TYPE;
 
 
-CREATE OR REPLACE PROCEDURE INCREASE_SALARY (position VARCHAR, percentage NUMBER)
-IS
+CREATE OR REPLACE PROCEDURE INCREASE_SALARY(position VARCHAR, percentage NUMBER)
+    IS
     old_salary EMPLOYEE.SALARY%type;
     employeeID EMPLOYEE.EMPLOYEE_ID%type;
     new_salary NUMBER;
-    CURSOR select_salary IS SELECT SALARY,EMPLOYEE_ID FROM EMPLOYEE WHERE EMPLOYEE.WORK_POSITION = position;
+    CURSOR select_salary IS SELECT SALARY, EMPLOYEE_ID
+                            FROM EMPLOYEE
+                            WHERE EMPLOYEE.WORK_POSITION = position;
 BEGIN
     OPEN select_salary;
     LOOP
         FETCH select_salary INTO old_salary,employeeID;
         EXIT WHEN select_salary%NOTFOUND;
-        new_salary := old_salary + (old_salary * (percentage/100));
+        new_salary := old_salary + (old_salary * (percentage / 100));
         UPDATE EMPLOYEE SET SALARY = new_salary WHERE EMPLOYEE.EMPLOYEE_ID = employeeID;
     end loop;
     CLOSE select_salary;
@@ -63,7 +67,8 @@ BEGIN
 end;
 /
 
-SELECT WORK_POSITION,SALARY FROM EMPLOYEE;
+SELECT WORK_POSITION, SALARY
+FROM EMPLOYEE;
 
 -- explicitní vytvoření alespoň jednoho indexu tak, aby pomohl optimalizovat zpracování dotazů,
 -- přičemž musí být uveden také příslušný dotaz, na který má index vliv,
